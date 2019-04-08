541e87595333c03b443c304626c88f090cd3dc04
sizer <shinse.tanaka@gmail.com>
2019-04-08 09:20:05 +0900

    delete app.css and write on app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    h1 { color: red; }
+  `]
 })
 export class AppComponent {
   title = 'ng-emotion';
