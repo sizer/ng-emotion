08187446453c6ffca2be82ff6245304f92a511e2
sizer <shinse.tanaka@gmail.com>
2019-04-08 20:38:26 +0900

    attach style on html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { css, cx } from 'emotion'
+
+const color = 'red';
+const hoverColor = 'blue';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div style="text-align:center">
-      <h1 [className]="h1Style | async">
+    <div  [className]="containerStyle">
+      <h1 [className]="h1Style">
         Welcome to {{ title }}!
       </h1>
       <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
@@ -13,4 +17,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-emotion';
+
+  containerStyle = css({
+    textAlign: 'center',
+    '&:hover': {
+      background: hoverColor,
+    }
+  });
+
+  h1Style = css({
+    color: color,
+  })
 }
